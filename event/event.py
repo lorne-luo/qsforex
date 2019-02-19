0cc4a9ad817a4b601529cd2bfce57001efb0122c
Lorne <lorne.luo@butterfly.com.au>
2019-02-19 11:09:31 +1100

    add Fill Event and oanda helper for order

--- event/event.py
+++ event/event.py
@@ -56,3 +56,68 @@ class OrderEvent(Event):
             str(self.type), str(self.instrument), str(self.units),
             str(self.order_type), str(self.side)
         )
+
+
+class FillEvent(Event):
+    """
+    When an ExecutionHandler receives an OrderEvent it must transact the order. Once an order has been transacted it generates a FillEvent, which describes the cost of purchase or sale as well as the transaction costs, such as fees or slippage.
+    The FillEvent is the Event with the greatest complexity. It contains a timestamp for when an order was filled, the symbol of the order and the exchange it was executed on, the quantity of shares transacted, the actual price of the purchase and the commission incurred.
+
+    Encapsulates the notion of a Filled Order, as returned
+    from a brokerage. Stores the quantity of an instrument
+    actually filled and at what price. In addition, stores
+    the commission of the trade from the brokerage.
+    """
+
+    def __init__(self, timeindex, symbol, exchange, quantity,
+                 direction, fill_cost, commission=None):
+        """
+        Initialises the FillEvent object. Sets the symbol, exchange,
+        quantity, direction, cost of fill and an optional
+        commission.
+
+        If commission is not provided, the Fill object will
+        calculate it based on the trade size and Interactive
+        Brokers fees.
+
+        Parameters:
+        timeindex - The bar-resolution when the order was filled.
+        symbol - The instrument which was filled.
+        exchange - The exchange where the order was filled.
+        quantity - The filled quantity.
+        direction - The direction of fill ('BUY' or 'SELL')
+        fill_cost - The holdings value in dollars.
+        commission - An optional commission sent from IB.
+        """
+
+        self.type = 'FILL'
+        self.timeindex = timeindex
+        self.symbol = symbol
+        self.exchange = exchange
+        self.quantity = quantity
+        self.direction = direction
+        self.fill_cost = fill_cost
+
+        # Calculate commission
+        if commission is None:
+            self.commission = self.calculate_ib_commission()
+        else:
+            self.commission = commission
+
+    def calculate_ib_commission(self):
+        """
+        Calculates the fees of trading based on an Interactive
+        Brokers fee structure for API, in USD.
+
+        This does not include exchange or ECN fees.
+
+        Based on "US API Directed Orders":
+        https://www.interactivebrokers.com/en/index.php?f=commission&p=stocks2
+        """
+        full_cost = 1.3
+        if self.quantity <= 500:
+            full_cost = max(1.3, 0.013 * self.quantity)
+        else:  # Greater than 500
+            full_cost = max(1.3, 0.008 * self.quantity)
+        full_cost = min(full_cost, 0.5 / 100.0 * self.quantity * self.fill_cost)
+        return full_cost
---
+++ oanda_v20/order.py
@@ -0,0 +1,10 @@
+from decimal import Decimal
+
+
+def lots_to_units(lot, side):
+    RATIO = 100000
+    if side == 'BUY':
+        return lot * RATIO
+    elif side == 'BUY':
+        return lot * RATIO * -1
+    raise Exception('Unknow direction.')
