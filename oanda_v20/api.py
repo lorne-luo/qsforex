32de9d4249e88d87a61be58e50aa351533697061
Lorne <lorne.luo@butterfly.com.au>
2019-02-19 16:37:43 +1100

    singleton V20 API context

--- oanda_v20/api.py
+++ oanda_v20/api.py
@@ -26,3 +26,25 @@ def create_streaming_context():
     )
 
     return ctx
+
+
+class SingletonDecorator:
+    def __init__(self, klass):
+        self.klass = klass
+        self.instance = None
+
+    def __call__(self, *args, **kwds):
+        if self.instance == None:
+            self.instance = self.klass(*args, **kwds)
+        return self.instance
+
+
+SingletonContext = SingletonDecorator(v20.Context)
+
+api = SingletonContext(hostname=settings.API_DOMAIN,
+                       application=settings.APPLICATION_NAME,
+                       token=settings.ACCESS_TOKEN, )
+
+stream_api = SingletonContext(hostname=settings.STREAM_DOMAIN,
+                              application=settings.APPLICATION_NAME,
+                              token=settings.ACCESS_TOKEN, )
