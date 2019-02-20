2debe0f4be0a8b44544d0ed18a62aabad9242364
Lorne <lorne.luo@butterfly.com.au>
2019-02-20 11:36:51 +1100

    improve candle getter

--- oanda_v20/candles.py
+++ oanda_v20/candles.py
@@ -1,27 +1,40 @@
+import logging
 from oanda_v20.api import api, stream_api
+import pandas as pd
+
+from oanda_v20.constants import get_symbol
+
+logger = logging.getLogger(__name__)
 
 
-def get_candle(instrument, granularity, count=50, fromTime=None, toTime=None, price_type='M',smooth=False):
+def get_candle(instrument, granularity, count=50, fromTime=None, toTime=None, price_type='M', smooth=False):
+    instrument = get_symbol(instrument)
     response = api.instrument.candles(instrument, granularity=granularity, count=count, fromTime=fromTime,
-                                      toTime=toTime, price=price_type,smooth=smooth)
+                                      toTime=toTime, price=price_type, smooth=smooth)
 
     if response.status != 200:
-        print(response)
-        print(response.body)
+        logger.error('[GET_Candle]',response,response.body)
         return []
 
     candles = response.get("candles", 200)
 
-    if price_type == 'M':
-        price = 'mid'
-    elif price_type == 'B':
+    price = 'mid'
+    if price_type == 'B':
         price = 'bid'
     elif price_type == 'A':
         price = 'ask'
 
     data = [[candle.time.split(".")[0],
-             getattr(candle, 'price', None).o,
-             getattr(candle, 'price', None).h,
-             getattr(candle, 'price', None).l,
-             getattr(candle, 'price', None).c]
-            for candle in response.get("candles", 200)]
+             getattr(candle, price, None).o,
+             getattr(candle, price, None).h,
+             getattr(candle, price, None).l,
+             getattr(candle, price, None).c,
+             candle.volume]
+            for candle in candles]
+
+    df = pd.DataFrame(data, columns=['time', 'open', 'high', 'low', 'open', 'volume'])
+    df['time'] = pd.to_datetime(df['time'])
+    df = df.set_index('time')
+    df.head()
+
+    return df
