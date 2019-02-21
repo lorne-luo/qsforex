01531b6018750364c9be64027d58ac518bec0ff2
Lorne <dev@luotao.net>
2019-02-21 14:35:39 +1100

    create market order in account

--- oanda_v20/account.py
+++ oanda_v20/account.py
@@ -429,3 +429,67 @@ class Account(EntityBase):
         else:
             log_error(logger, response, 'CLOSE_POSITION')
             return False, response.body['errorMessage']
+
+    # ================= ORDER =================
+    def market_order(self, instrument, side,
+                     lots=0.1, type=OrderType.MARKET, timeInForce=TimeInForce.FOK,
+                     priceBound=None, positionFill=OrderPositionFill.DEFAULT,
+                     take_profit_price=None,
+                     stop_loss_pip=None,
+                     trailing_pip=None,
+                     client_id=None, client_tag=None, client_comment=None):
+        instrument = get_symbol(instrument)
+        units = lots_to_units(lots, side)
+        pip_unit = self.get_pip_unit(instrument)
+
+        # client extension
+        client_args = {'id': client_id, 'tag': client_tag, 'comment': client_comment}
+        if any(client_args.values()):
+            tradeClientExtensions = ClientExtensions(**client_args)
+        else:
+            tradeClientExtensions = None
+
+        # stop loss
+        if stop_loss_pip:
+            stop_loss_price = pip_unit * Decimal(str(stop_loss_pip))
+            stop_loss_details = StopLossDetails(distance=str(stop_loss_price), clientExtensions=tradeClientExtensions)
+        else:
+            stop_loss_details = None
+
+        take_profit_detail = TakeProfitDetails(price=str(take_profit_price), clientExtensions=tradeClientExtensions)
+
+        if trailing_pip:
+            trailing_distance_price = pip_unit * Decimal(str(trailing_pip))
+            trailing_details = TrailingStopLossDetails(distance=str(trailing_distance_price),
+                                                       clientExtensions=tradeClientExtensions)
+        else:
+            trailing_details = None
+
+        response = self.api.order.market(
+            self.account_id,
+            instrument=instrument, units=str(units), type=type, timeInForce=timeInForce,
+            priceBound=priceBound, positionFill=positionFill,
+            takeProfitOnFill=take_profit_detail,
+            stopLossOnFill=stop_loss_details,
+            trailingStopLossOnFill=trailing_details,
+            tradeClientExtensions=tradeClientExtensions
+        )
+
+        if response.status >= 200:
+            transactions = []
+            for name in TransactionName.all():
+                try:
+                    transaction = response.get(name, None)
+                    transactions.append(transaction)
+                except:
+                    pass
+            for t in transactions:
+                common_view.print_entity(t, title=t.__class__.__name__)
+                print('')
+
+            # todo fail: ORDER_CANCEL,MARKET_ORDER_REJECT
+            # todo success:MARKET_ORDER + ORDER_FILL
+            return transactions
+        else:
+            log_error(logger, response, 'MARKET_ORDER')
+            return False, response.body.get('errorMessage')
--- oanda_v20/order.py
+++ oanda_v20/order.py
@@ -1,6 +1,8 @@
 import logging
 from decimal import Decimal, ROUND_HALF_UP
 
+from v20.transaction import StopLossDetails, ClientExtensions, TakeProfitDetails, TrailingStopLossDetails
+
 from oanda_v20.api import api
 from oanda_v20.common.view import print_entity
 from oanda_v20.convertor import get_symbol, lots_to_units
@@ -11,12 +13,29 @@ logger = logging.getLogger(__name__)
 
 
 def create_market_order(instrument, side, lots=0.1, type=OrderType.MARKET, timeInForce=TimeInForce.FOK,
-                        priceBound=None, positionFill=OrderPositionFill.DEFAULT, clientExtensions=None,
+                        priceBound=None, positionFill=OrderPositionFill.DEFAULT,
                         takeProfitOnFill=None, stopLossOnFill=None, trailingStopLossOnFill=None,
-                        tradeClientExtensions=None):
+                        client_id=None, client_tag=None, client_comment=None):
     instrument = get_symbol(instrument)
-
     units = lots_to_units(lots, side)
+
+
+    if stopLossOnFill:
+        stopLossOnFill = StopLossDetails(price=str(stopLossOnFill))
+
+    if takeProfitOnFill:
+        takeProfitOnFill = TakeProfitDetails(price=str(takeProfitOnFill))
+
+    if trailingStopLossOnFill:
+        trailingStopLossOnFill = TrailingStopLossDetails(distance=str(trailingStopLossOnFill))
+
+    # client extension
+    client_args = {'id': client_id, 'tag': client_tag, 'comment': client_comment}
+    if any(client_args.values()):
+        tradeClientExtensions = ClientExtensions(**client_args)
+    else:
+        tradeClientExtensions = None
+
     response = api.order.market(
         settings.ACCOUNT_ID,
         instrument=instrument, units=str(units), type=type, timeInForce=timeInForce,
@@ -36,6 +55,8 @@ def create_market_order(instrument, side, lots=0.1, type=OrderType.MARKET, timeI
         for t in transactions:
             print_entity(t, title=t.__class__.__name__)
             print('')
+
+        #todo fail: ORDER_CANCEL,MARKET_ORDER_REJECT
         return transactions
     else:
         logger.error(
@@ -46,3 +67,7 @@ def create_market_order(instrument, side, lots=0.1, type=OrderType.MARKET, timeI
             )
         )
         return False, response.body.get('errorMessage')
+
+
+# from oanda_v20.order import *
+# response=create_market_order('EUR_USD','BUY',stopLossOnFill='1.3145',client_tag='test',client_comment='test')
