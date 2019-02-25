f2abf0328250aca5b484168e31bed949dd5af419
Lorne <lorne.luo@butterfly.com.au>
2019-02-25 16:19:12 +1100

    order cancel and list

--- oanda_v20/common/logger.py
+++ oanda_v20/common/logger.py
@@ -1,9 +1,9 @@
 def log_error(logger, response, title):
     logger.error(
-        "[{}] {}, {}, {}\n".format(
+        "[{}] {}#{}, {}\n".format(
             title,
             response.status,
-            response.body.get('errorCode') or response.body.get('errorMessage'),
-            response.body.get('errorMessage'),
+            response.body.get('errorCode') or '',
+            response.body.get('errorMessage') or response.body,
         )
     )
--- oanda_v20/order.py
+++ oanda_v20/order.py
@@ -4,7 +4,8 @@ from decimal import Decimal, ROUND_HALF_UP
 from v20.transaction import StopLossDetails, ClientExtensions, TakeProfitDetails, TrailingStopLossDetails
 from oanda_v20.base import api, EntityBase
 from oanda_v20.common.logger import log_error
-from oanda_v20.common.view import print_entity
+from oanda_v20.common.prints import print_orders
+from oanda_v20.common.view import print_entity, print_response_entity
 from oanda_v20.common.convertor import get_symbol, lots_to_units
 from oanda_v20.common.constants import TransactionName, OrderType, OrderPositionFill, TimeInForce
 import settings
@@ -13,6 +14,32 @@ logger = logging.getLogger(__name__)
 
 
 class OrderMixin(EntityBase):
+
+    # order list
+    def _process_orders(self, response):
+        orders = response.get("orders", "200")
+        if response.status < 200 or response.status > 299:
+            log_error(logger, response, 'LIST_ORDER')
+            return False, response.body.get('errorMessage')
+
+        if len(orders) == 0:
+            logger.debug("Account {} has no pending Orders to cancel".format(
+                self.account_id
+            ))
+        for order in orders:
+            self.orders[order.id] = order
+
+        if settings.DEBUG:
+            print_orders(orders)
+        return True, orders
+
+    def list_order(self, ids=None, state=None, instrument=None, count=None, beforeID=None):
+        response = api.order.list(self.account_id, ids, state, instrument, count, beforeID)
+        return self._process_orders(response)
+
+    def list_pending_order(self):
+        response = api.order.list_pending(self.account_id)
+        return self._process_orders(response)
 
     def market_order(self, instrument, side,
                      lots=0.1, type=OrderType.MARKET, timeInForce=TimeInForce.FOK,
@@ -39,7 +66,10 @@ class OrderMixin(EntityBase):
         else:
             stop_loss_details = None
 
-        take_profit_detail = TakeProfitDetails(price=str(take_profit_price), clientExtensions=tradeClientExtensions)
+        if take_profit_price:
+            take_profit_detail = TakeProfitDetails(price=str(take_profit_price), clientExtensions=tradeClientExtensions)
+        else:
+            take_profit_detail = None
 
         if trailing_pip:
             trailing_distance_price = pip_unit * Decimal(str(trailing_pip))
@@ -77,3 +107,20 @@ class OrderMixin(EntityBase):
         # todo success:MARKET_ORDER + ORDER_FILL
         return True, transactions
 
+    # cancel
+    def cancel_order(self, order_id):
+        response = api.order.cancel(self.account_id, order_id)
+
+        print("Response: {} ({})".format(response.status, response.reason))
+
+        if response.status < 200 or response.status > 299:
+            transaction = response.get('orderCancelRejectTransaction', "404")
+            if settings.DEBUG:
+                print_entity(transaction, title='Order Cancel Reject')
+            return False, transaction
+
+        transaction = response.get('orderCancelTransaction', "200")
+        if settings.DEBUG:
+            print_entity(transaction, title='Order Canceled')
+
+        return True, transaction
