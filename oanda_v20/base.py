829540e7c25ec4c3d08cd2019b19b74f96a310e8
Lorne <lorne.luo@butterfly.com.au>
2019-02-28 10:35:11 +1100

    sington  keep multiple instance

--- oanda_v20/base.py
+++ oanda_v20/base.py
@@ -32,25 +32,30 @@ def create_streaming_context():
 class SingletonDecorator:
     def __init__(self, klass):
         self.klass = klass
-        self.instance = None
+        self.instance = {}
 
-    def __call__(self, *args, **kwds):
-        if self.instance == None:
-            self.instance = self.klass(*args, **kwds)
-        return self.instance
+    def __call__(self, *args, **kwargs):
+        hostname = kwargs.get('hostname')
+        token = kwargs.get('token')
+        key = '%s-%s' % (hostname, token)
+        if 'application' not in kwargs:
+            kwargs['application'] = settings.APPLICATION_NAME
+
+        if self.instance.get(key, None) == None:
+            self.instance[key] = self.klass(*args, **kwargs)
+        return self.instance[key]
 
 
 SingletonAPIContext = SingletonDecorator(v20.Context)
-SingletonStreamAPIContext = SingletonDecorator(v20.Context)
 
 api = SingletonAPIContext(hostname=settings.API_DOMAIN,
-                       application=settings.APPLICATION_NAME,
-                       token=settings.ACCESS_TOKEN, )
+                          application=settings.APPLICATION_NAME,
+                          token=settings.ACCESS_TOKEN, )
 
-stream_api = SingletonStreamAPIContext(hostname=settings.STREAM_DOMAIN,
-                              application=settings.APPLICATION_NAME,
-                              token=settings.ACCESS_TOKEN,
-                              datetime_format='RFC3339')
+stream_api = SingletonAPIContext(hostname=settings.STREAM_DOMAIN,
+                                 application=settings.APPLICATION_NAME,
+                                 token=settings.ACCESS_TOKEN,
+                                 datetime_format='RFC3339')
 
 
 class EntityBase(object):
