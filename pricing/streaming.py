9bdd01974a42a56bad13cd2b643dce11299b57aa
Lorne <lorne.luo@butterfly.com.au>
2019-02-27 16:46:50 +1100

    main runner for oanda v20 streaming

--- pricing/streaming.py
+++ pricing/streaming.py
@@ -1,13 +1,25 @@
 from __future__ import print_function
 
+from datetime import datetime
 from decimal import Decimal, getcontext, ROUND_HALF_DOWN
 import logging
 import json
+from queue import Empty
 
+import dateparser
 import requests
+import v20
 
 from qsforex.event.event import TickEvent
 from qsforex.pricing.price import PriceHandler
+
+import settings
+from event.event import HeartBeatEvent, TickPriceEvent
+from oanda_v20.common.convertor import get_symbol
+from runner.base import StreamRunnerBase
+from settings import OANDA_ENVIRONMENTS
+
+logger = logging.getLogger(__name__)
 
 
 class StreamingForexPrices(PriceHandler):
@@ -95,3 +107,78 @@ class StreamingForexPrices(PriceHandler):
                     self.prices[inv_pair]["time"] = time
                     tev = TickEvent(instrument, time, bid, ask)
                     self.events_queue.put(tev)
+
+
+class OandaV20StreamRunner(StreamRunnerBase):
+    DEFAULT_CURRENCIES = ['EUR_USD', 'GBP_USD', 'USD_JPY', 'USD_CHF', 'AUD_USD', 'NZD_USD', 'USD_CNH', 'XAU_USD']
+
+    def __init__(self, queue, pairs, domain, access_token, account_id, *args, **kwargs):
+        super(StreamRunnerBase, self).__init__(queue)
+        self.pairs = pairs
+        self.prices = self._set_up_prices_dict()
+        self.domain = domain
+        self.access_token = access_token
+        self.account_id = account_id
+        self.register(*args)
+
+    def run(self):
+        print('%s statup.' % self.__class__.__name__)
+        print('Registered handler: %s' % ', '.join([x.__class__.__name__ for x in self.handlers]))
+
+        pairs_oanda = [get_symbol(p) for p in self.pairs]
+        pair_list = ",".join(pairs_oanda)
+        print('Pairs:', pair_list)
+        print('\n')
+
+        stream_api = v20.Context(
+            hostname=OANDA_ENVIRONMENTS["streaming"][self.domain],
+            application=settings.APPLICATION_NAME,
+            token=self.access_token,
+            datetime_format='RFC3339'
+        )
+
+        response = stream_api.pricing.stream(
+            self.account_id,
+            instruments=pair_list,
+            snapshot=True,
+        )
+
+        for msg_type, msg in response.parts():
+            if msg_type == "pricing.PricingHeartbeat":
+                self.put(HeartBeatEvent())
+            elif msg_type == "pricing.ClientPrice" and msg.type == 'PRICE':
+                instrument = msg.instrument
+                time = dateparser.parse(msg.time)
+                bid = Decimal(str(msg.bids[0].price))
+                ask = Decimal(str(msg.asks[0].price))
+                self.put(TickPriceEvent(instrument, time, bid, ask))
+            else:
+                print('Unknow type:', msg_type, msg.__dict__)
+
+            try:
+                event = self.queue.get(False)
+            except Empty:
+                self.put(HeartBeatEvent())
+            else:
+                if event:
+                    logger.debug("Received new %s event: %s", (event.type, event.__dict__))
+                    self.process_event(event)
+
+
+class BacktestRunner(StreamRunnerBase):
+    def __init__(self, queue, pairs, csv, *args, **kwargs):
+        super(BacktestRunner, self).__init__(queue)
+        self.pairs = pairs
+        self.register(*args)
+        # todo backtest with csv
+
+
+if __name__ == '__main__':
+    from event.runner import *
+    from event.handler import *
+    import queue, settings
+
+    q = queue.Queue()
+    d = DebugHandler(q)
+    r = OandaV20StreamRunner(q, ['EUR_USD', 'GBP_USD'], 'practice', settings.ACCESS_TOKEN, settings.ACCOUNT_ID, d)
+    r.run()
