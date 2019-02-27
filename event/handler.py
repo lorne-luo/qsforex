60692ae8bb356f6c1fb9357aeed519f1a8f9eeb5
Lorne <lorne.luo@butterfly.com.au>
2019-02-27 16:45:42 +1100

    main runner and event handler

---
+++ event/handler.py
@@ -0,0 +1,98 @@
+import copy
+import logging
+from datetime import datetime
+
+from event.event import *
+from mt4.constants import PERIOD_CHOICES, get_candle_time
+
+logger = logging.getLogger(__name__)
+
+
+class QueueBase(object):
+    queue = None
+
+    def __init__(self, queue):
+        self.queue = queue
+
+    def set_queue(self, queue):
+        if not self.queue:
+            self.queue = queue
+
+    def put(self, event):
+        if self.queue:
+            self.queue.put(event)
+        else:
+            logger.error('Handler do not have queue')
+
+
+class BaseHandler(QueueBase):
+    subscription = []
+
+    def process(self, event):
+        raise NotImplementedError
+
+
+class DebugHandler(BaseHandler):
+    subscription = ['*']
+
+    def process(self, event):
+        print(event.__dict__)
+
+
+class TickHandler(BaseHandler):
+    def process(self, event):
+        pass
+
+
+class HeartBeatPrintHandler(BaseHandler):
+    subscription = [HeartBeatEvent]
+
+    def process(self, event):
+        if self.DEBUG:
+            print('HeartBeat: %s' % datetime.utcnow())
+
+
+class TimeFrameTicker(BaseHandler):
+    subscription = [HeartBeatEvent]
+    candle_time = {}
+    market_open = False
+    HOLIDAY = [(1, 1)]
+
+    def __init__(self, queue=None):
+        super(TimeFrameTicker, self).__init__(queue)
+
+        now = datetime.utcnow()
+        for timeframe in PERIOD_CHOICES:
+            self.candle_time[timeframe] = get_candle_time(now, timeframe)
+
+    def get_market_open(self, now):
+        # GBP/USD,20181207 20:59:58.156,1.27418,1.27426
+        # GBP/USD,20181209 22:02:52.967,1.27017,1.27138
+        if now.weekday() == 5:
+            return False
+        if now.weekday() == 4:
+            return now.hour < 20
+        if now.weekday() == 6:
+            return now.hour > 22
+
+        for date in self.HOLIDAY:
+            if (now.day, now.month) == date:
+                return now.hour < 20 or now.hour > 22
+        return True
+
+    def process(self, event):
+        now = datetime.utcnow()
+        for timeframe in PERIOD_CHOICES:
+            new = get_candle_time(now, timeframe)
+            if self.candle_time[timeframe] != new:
+                self.put(TimeFrameEvent(timeframe))
+                print(self.candle_time[timeframe], new)
+                self.candle_time[timeframe] = new
+
+        open = self.get_market_open(now)
+        if self.market_open != open:
+            if open:
+                self.put(MarketOpenEvent())
+            else:
+                self.put(MarketCloseEvent())
+            self.market_open = open
---
+++ event/runner.py
@@ -0,0 +1,91 @@
+import logging
+import queue
+import time
+
+import settings
+from event.event import Event
+from event.handler import QueueBase, HeartBeatEvent, BaseHandler
+
+logger = logging.getLogger(__name__)
+
+
+class Runner(QueueBase):
+    handlers = []
+
+    def run(self):
+        raise NotImplementedError
+
+    def register(self, *args):
+        for handler in args:
+            if isinstance(handler, BaseHandler):
+                handler.set_queue(self.queue)
+                self.handlers.append(handler)
+
+    def process_event(self, event):
+        for handler in self.handlers:
+            if '*' in handler.subscription:
+                handler.process(event)
+                break
+            for event_type in handler.subscription:
+                if isinstance(event, event_type):
+                    handler.process(event)
+                    break
+
+    def print(self):
+        print(self.handlers)
+
+
+class HeartbeatRunner(Runner):
+    def __init__(self, queue, heartbeat=5, *args, **kwargs):
+        super(HeartbeatRunner, self).__init__(queue)
+        self.heartbeat = heartbeat or 5  # seconds
+        self.register(*args)
+
+    def run(self):
+        print('%s statup.' % self.__class__.__name__)
+        print('Registered handler: %s' % ', '.join([x.__class__.__name__ for x in self.handlers]))
+        print('\n')
+
+        while True:
+            try:
+                event = self.queue.get(False)
+            except queue.Empty:
+                time.sleep(self.heartbeat)
+                self.put(HeartBeatEvent())
+            else:
+                if event:
+                    if settings.DEBUG:
+                        print("Received new %s event: %s", (event.type, event.__dict__))
+                    else:
+                        logger.debug("Received new %s event: %s", (event.type, event.__dict__))
+
+                    self.process_event(event)
+
+
+class StreamRunnerBase(Runner):
+    def __init__(self, queue, pairs, *args, **kwargs):
+        super(StreamRunnerBase, self).__init__(queue)
+        self.register(*args)
+        self.pairs = pairs
+        self.prices = self._set_up_prices_dict()
+
+    def _set_up_prices_dict(self):
+        prices_dict = dict(
+            (k, v) for k, v in [
+                (p, {"bid": None, "ask": None, "time": None, "spread": None}) for p in self.pairs
+            ]
+        )
+
+        return prices_dict
+
+
+if __name__ == '__main__':
+    # python -m event.runner
+    from event.handler import *
+    import queue
+
+    q = queue.Queue()
+    d = DebugHandler(q)
+    t = TimeFrameTicker(q)
+    r = HeartbeatRunner(q, 5, d, t)
+    r.run()
