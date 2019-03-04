47394ee02ded106b5b71875d0ae8d817c023d131
Lorne <lorne.luo@butterfly.com.au>
2019-03-04 15:55:14 +1100

    strategy base

--- strategy/base.py
+++ strategy/base.py
@@ -1,25 +1,41 @@
-class StrategyBase(object):
+from datetime import datetime
+
+from event.handler import QueueBase
+from utils.market import is_market_open
+
+
+class StrategyBase(QueueBase):
     name = None
     version = None
+    magic_number = None
+    source = None
     queue = None
 
-    weekdays = list(range(6))
+    weekdays = [0, 1, 2, 3, 4]  # Mon to Fri
+    timeframes = []
+    hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]  # GMT hour
 
-    pairs = {
-        'EURUSD': {'timeframes': [], 'longest_window': 30, 'hours': list(range(24))}  # GMT hour
-    }
-
+    pairs = []
     data_reader = None
+    params = {}
 
     def __str__(self):
-        return '%s v%s' % (self.name, self.version)
+        return '%s v%s #%s' % (self.name, self.version, self.magic_number)
 
     def __init__(self, reader, queue, *args, **kwargs):
+        super(StrategyBase, self).__init__(queue)
         self.data_reader = reader
-        self.queue = queue
 
     def calculate_signals(self):
         raise NotImplementedError
 
-    def put_event(self, event):
-        self.queue.put(event)
+    def can_open_trade(self):
+        if not is_market_open():
+            return False
+
+        now = datetime.utcnow()
+        if now.weekday() not in self.weekdays:
+            return False
+        if now.hour not in self.weekdays:
+            return False
+        return True
