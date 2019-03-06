4edd679e3407a2aa030f1f4f99d9d758864f435a
Lorne <dev@luotao.net>
2019-03-06 19:13:23 +1100

    add shift for helper

--- strategy/helper.py
+++ strategy/helper.py
@@ -1,16 +1,22 @@
 from mt4.constants import OrderSide
 
 
-def check_cross(data1, data2):
-    if data1[-2] > data2[-2] and data1[-3] < data2[-3]:
+def check_cross(data1, data2, shift=1):
+    last = -1 - shift
+    previous = -2 - shift
+    if data1[last] > data2[last] and data1[previous] < data2[previous]:
         return OrderSide.BUY
-    if data1[-2] < data2[-2] and data1[-3] > data2[-3]:
+    if data1[last] < data2[last] and data1[previous] > data2[previous]:
         return OrderSide.SELL
     return None
 
-def check_reverse(data):
-    if data[-1] > data[-2] < data[3]:
+
+def check_reverse(data, shift=1):
+    first = -1 - shift
+    second = -2 - shift
+    third = -3 - shift
+    if data[first] > data[second] < data[third]:
         return OrderSide.BUY
-    if data[-1] < data[-2] > data[3]:
+    if data[first] < data[second] > data[third]:
         return OrderSide.SELL
     return None
