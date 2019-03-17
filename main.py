1050807b8125e558672e22ff6581f7853159016f
Lorne <lorne.luo@butterfly.com.au>
2019-03-18 10:19:47 +1100

    update logging settings

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import logging
 from queue import Queue
 
 from broker.fxcm.streaming import FXCMStreamRunner
 from event.event import TickPriceEvent
 from event.handler import DebugHandler, TimeFrameTicker, TimeFrameEvent, TickPriceHandler
 from utils.price_density import PriceDensityHandler
+
+logging.FileHandler('/opt/qsforex/log/main.log')
 
 queue = Queue()
 YOURTOKEN = '8a1e87908a70362782ea9744e2c9c82689bde3ac'
@@ -15,4 +18,5 @@ price_density = PriceDensityHandler(queue, pairs)
 
 runner = FXCMStreamRunner(queue, pairs=pairs, access_token=YOURTOKEN,
                           handlers=[tick_price_handler, timeframe_ticker, price_density])
+logging.info('%s started' % runner.__class__.__name__)
 runner.run()
