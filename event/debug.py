74519f41eafa0affc8580a7f048da0cf5fb653f7
Lorne <lorne.luo@butterfly.com.au>
2019-04-15 15:52:37 +1000

    shortcut for debug cmd

--- event/debug.py
+++ event/debug.py
@@ -18,3 +18,43 @@ def connect_debug(action, queue='FXCM'):
     event = ConnectEvent(action)
     data = json.dumps(event.to_dict())
     queue.put(data)
+
+
+def order():
+    debug('order')
+
+
+def trade():
+    debug('trade')
+
+
+def account():
+    debug('account')
+
+
+def connect():
+    connect_debug('connect')
+
+
+def reconnect():
+    connect_debug('reconnect')
+
+
+def disconnect():
+    connect_debug('disconnect')
+
+
+def status():
+    connect_debug('status')
+
+
+def market_close():
+    connect_debug('market_close')
+
+
+def market_open():
+    connect_debug('market_open')
+
+
+def new_connect():
+    connect_debug('new_connect')
