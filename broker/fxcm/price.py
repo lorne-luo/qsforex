06be8d245f05f89a59ed9fb5c63478a942a0d310
Lorne <lorne.luo@butterfly.com.au>
2019-04-24 10:10:19 +1000

    refine get_dataframe by time range

--- broker/fxcm/price.py
+++ broker/fxcm/price.py
@@ -2,10 +2,12 @@ import json
 import logging
 from decimal import Decimal
 import pandas as pd
+from dateutil.relativedelta import relativedelta
+
 from broker.base import PriceBase
 from broker.fxcm.constants import get_fxcm_symbol, get_fxcm_timeframe
 from broker.oanda.base import OANDABase
-from mt4.constants import get_mt4_symbol, pip
+from mt4.constants import get_mt4_symbol, pip, get_candle_time
 from utils.redis import price_redis, get_tick_price
 
 logger = logging.getLogger(__name__)
@@ -56,23 +58,19 @@ class PriceMixin(OANDABase, PriceBase):
 
     def get_dataframe(self, instrument, granularity, fromTime, toTime):
         instrument = get_fxcm_symbol(instrument)
+        end = get_candle_time(toTime, granularity)
+        fromTime = get_candle_time(fromTime, granularity)
         granularity = get_fxcm_timeframe(granularity)
-        granularity_seconds = int(granularity)
-        end = toTime
         result = None
 
         while end > fromTime:
-            seconds = (end - fromTime).seconds
-            if seconds / granularity_seconds > self.MAX_CANDLES:
-                data = self.fxcmpy.get_candles(instrument, period=granularity, number=self.MAX_CANDLES, end=toTime,
-                                               columns=['askhigh', 'bidlow', 'tickqty'])
-            else:
-                data = self.fxcmpy.get_candles(instrument, period=granularity, start=fromTime, end=toTime,
-                                               columns=['askhigh', 'bidlow', 'tickqty'])
+            data = self.fxcmpy.get_candles(instrument, period=granularity, number=self.MAX_CANDLES, end=end,
+                                           columns=['askhigh', 'askopen', 'bidclose', 'bidlow', 'tickqty'])
             if result is not None:
-                result = pd.concat([result, data])
+                result = pd.concat([result, data], sort=True).sort_index()
             else:
                 result = data
-            end = result.tail.name.to_pydatetime()
 
-        return result
+            end = result.iloc[0].name.to_pydatetime() - relativedelta(seconds=10)
+        result.columns = ['high', 'open', 'close', 'low', 'volume']
+        return result[result.index >= fromTime]
